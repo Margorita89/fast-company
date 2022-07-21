115e16516fed9a5aae9d5a358f5cbf32b532cfc7
Margarita Kishinevskaya <kishinevskaya1999@mail.ru>
2022-07-21 14:52:20 +0500

    Checking protypes

--- src/app/App.js
+++ src/app/App.js
@@ -1,36 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Users from "./components/users";
-import SearchStatus from "./components/searchStatus"
-import api from "./API"
+import SearchStatus from "./components/searchStatus";
+import api from "./API";
 
 const App = () => {
-    const [users, setUsers] = useState(api.users.fetchAll())
+    const [users, setUsers] = useState(api.users.fetchAll());
 
     const handleDelete = (userId) => {
-        setUsers(users.filter(user => user._id !== userId))
-    }
+        setUsers(users.filter((user) => user._id !== userId));
+    };
 
     const handleToggleBookMark = (id) => {
-        setUsers(users.map(c => {
-            if (c._id === id) {
-                return {...c, bookmark: !c.bookmark}
-            } else {
-                return c
-            }
-        }))
-    }
+        setUsers(
+            users.map((c) => {
+                if (c._id === id) {
+                    return { ...c, bookmark: !c.bookmark };
+                } else {
+                    return c;
+                }
+            })
+        );
+    };
 
     return (
         <>
-            <SearchStatus length={users.length}/>
+            <SearchStatus length={users.length} />
             <Users
                 users={users}
-                onDelete ={handleDelete}
+                onDelete={handleDelete}
                 onToggle={handleToggleBookMark}
             />
         </>
-    )
-
+    );
 };
 
 export default App;
